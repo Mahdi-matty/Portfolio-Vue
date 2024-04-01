b107ba184d2633b579f05868180fa4682c9a4df8
mahdi <mmiq69@gmail.com>
2024-04-01 15:16:22 -0400

    initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Mahdi from "../assets/mahdi.jpg";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+    <div class="about">
+      <img :src="Mahdi" />
+      <h3>Mahdi Mohammadi</h3>
+      <p>Full stack developer</p>
+    </div>
+    <div>
+      <h3>Education</h3>
+      <p>BS.c industrial engineering</p>
+      <p>Full stack cooding bootcamp</p>
+    </div>
+    <div>
+      <h3>Skills:</h3>
+      <p>React</p>
+      <p>Mysql/Sequlize</p>
+      <p>Node.js</p>
+      <p>Vue.js</p>
+      <p>MongoDB</p>
+      <p>Express</p>
+    </div>
+  </div>
+  
   </main>
 </template>
